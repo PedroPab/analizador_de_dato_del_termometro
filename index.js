c293cf72e92306fc5333690eac8a46373df7e00c
Pedro Pablo <pmontesarroyave@gmail.com>
2022-08-26 21:12:55 -0500

    la cuadricula esta echa

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const API = 'https://iot-domiburguer.herokuapp.com/api/IOT/DOMIBURGER/FREIDORA/thermometer'
+
 const section_parrafo = document.getElementById('section_parrafo')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
 const time_long = 1000
+
+drawGrid()
 
 async function temperature(api) {
     const res = await fetch(api)
     const data = await res.json()
 
     if (res.status !== 200) {
-        console.log('erre ', res.status)
+        console.error('error ', res.status)
     } else {
         return data
     }
@@ -24,4 +30,29 @@ const drawTemperature = async () => {
     }, time_long)
 }
 
-drawTemperature()
+function timeApp() {
+    const date = new Date()
+    const [hour, minutes, seconds] = [date.getHours(), date.getMinutes(), date.getSeconds()]
+    return [hour, minutes, seconds]
+}
+function drawLine(x_i, y_i, x_f, y_f, color = '#A6A6A6') {
+    ctx.lineWidth = 1
+    ctx.strokeStyle = color
+    ctx.beginPath()
+    ctx.moveTo(x_i, y_i)
+    ctx.lineTo(x_f, y_f)
+    ctx.stroke()
+}
+
+function drawGrid() {
+    const height = canvas.height
+    const width = canvas.width
+    const density = 10
+    for (let i = 0; i < Math.floor(height / density) + 1; i++) {
+        drawLine(0, i * density, width, i * density )
+    }
+    for (let i = 0; i < Math.floor(width / density) + 1; i++) {
+        drawLine( i * density, 0, i * density, height  )
+    }
+}
+
